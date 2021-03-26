7a6a2f9fe5253c298bb6e9375ddee25a077165d2
Josafá Dieb <josafadieb@gmail.com>
2021-03-26 19:17:06 -0300

    Alteração de Teste

--- main.c
+++ main.c
@@ -5,11 +5,5 @@
 int main(){
     Biblioteca *biblioteca = malloc(sizeof(Biblioteca));
     cadastrarLivro(0, 5, 1, "Como fazer xxx em yyy pra ter zzz.", "aprenda a fazer xxx, em yyy para ter zzz", "João grandao");
-    cadastrarLivro(1, 5, 1, "Como fazer xxx em yyy pra ter zzz.", "aprenda a fazer xxx, em yyy para ter zzz", "João grandao");
-    cadastrarLivro(2, 5, 1, "Como fazer xxx em yyy pra ter zzz.", "aprenda a fazer xxx, em yyy para ter zzz", "João grandao");
-    cadastrarLivro(3, 5, 1, "Como fazer xxx em yyy pra ter zzz.", "aprenda a fazer xxx, em yyy para ter zzz", "João grandao");
-    cadastrarLivro(4, 5, 1, "Como fazer xxx em yyy pra ter zzz.", "aprenda a fazer xxx, em yyy para ter zzz", "João grandao");
-    cadastrarLivro(5, 5, 1, "Como fazer xxx em yyy pra ter zzz.", "aprenda a fazer xxx, em yyy para ter zzz", "João grandao");
-    cadastrarLivro(6, 5, 1, "Como fazer xxx em yyy pra ter zzz.", "aprenda a fazer xxx, em yyy para ter zzz", "João grandao");
     mostrarListaDeLivros();
 }
